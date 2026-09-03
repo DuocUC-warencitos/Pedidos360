244a8848af91d3aaa0783b0429449578f1f457c8
Roony11-1 <eltheasasin@gmail.com>
2026-09-03 12:54:36 -0400

    añadido microservicio de pedidos configurado en esquema base

---
+++ backend/pedidos-gateway/src/test/java/io/github/roony11_1/pedidos_gateway/PedidosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.roony11_1.pedidos_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
