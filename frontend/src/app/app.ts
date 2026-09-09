b4eeffe80deeb6eac67b0373b98559390176bc16
Roony11-1 <eltheasasin@gmail.com>
2026-09-09 00:14:29 -0300

    refactor: cambiada dependencias del header a su propio componente

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
+import { HeaderComponent } from './shared/component/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
