35538ee3d075af0f50bb13b334ba240cbcc10f8f
Roony11-1 <eltheasasin@gmail.com>
2026-09-16 04:56:39 -0300

    feat: initialize producto-service with Maven configuration and basic application structure

---
+++ backend/producto-service/src/test/java/io/github/roony11_1/producto_service/ProductoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.roony11_1.producto_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
