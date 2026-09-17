ea653bffc4cdbea82857fb5d7106ae324d1d382c
Roony11-1 <eltheasasin@gmail.com>
2026-09-17 02:01:58 -0300

    refactor(mapper): rename generic type parameter for clarity in IMapper interface

--- backend/pedidos-service/src/main/java/io/github/roony11_1/pedidos_service/core/domain/mapper/IMapper.java
+++ backend/pedidos-service/src/main/java/io/github/roony11_1/pedidos_service/core/domain/mapper/IMapper.java
@@ -1,7 +1,7 @@
 package io.github.roony11_1.pedidos_service.core.domain.mapper;
 
 @FunctionalInterface 
-public interface IMapper<T, E>
+public interface IMapper<T, R>
 {
-    T map(E entity);
+    T map(R source);
 }
